a61b587473df98b8752f70b5da410cf92ef500a3
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-01-23 23:38:22 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,38 @@
+Contao Extension: TinyMceInsertDateTime
+=======================================
+
+Special TinyMCE plugin to add date and time buttons to the editor.
+
+The sources of this plugin could be found [here](http://www.tinymce.com/wiki.php/Plugin:insertdatetime).
+
+
+Installation
+------------
+
+The extension is not published in contao extension repository.
+Install it manually.
+
+
+Tracker
+-------
+
+https://github.com/cliffparnitzky/TinyMceInsertDateTime/issues
+
+
+Compatibility
+-------------
+
+- min. version: Contao 3.0.x
+- max. version: Contao 3.2.x
+
+
+Dependency
+----------
+
+- To load this plugin and add it to the configuration the extension [[TinyMcePluginLoader]](https://github.com/cliffparnitzky/TinyMcePluginLoader) has to be installed.
+
+
+Screenshot
+----------
+
+![Screenshot](https://raw.github.com/cliffparnitzky/TinyMceInsertDateTime/master/screenshot.jpg)
---
+++ system/modules/TinyMceInsertDateTime/TinyMceInsertDateTime.php
@@ -0,0 +1,90 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2014
+ * @author     Cliff Parnitzky
+ * @package    TinyMceInsertDateTime
+ * @license    LGPL
+ */
+
+/**
+* Class TinyMceInsertDateTime
+*
+* Class to implement the HOOK for adding configs.
+* @copyright  Cliff Parnitzky 2014
+* @author     Cliff Parnitzky
+*/
+class TinyMceInsertDateTime {
+	
+	// mapping table to transform PHP date format into prefered format of TinyMCE
+	private $arrMapping = array(
+		'd' => '%d',
+		'D' => '%a',
+		'j' => '%d',
+		'l' => '%A',
+		'F' => '%B',
+		'm' => '%m',
+		'M' => '%b',
+		'n' => '%m',
+		'o' => '%Y',
+		'Y' => '%Y',
+		'y' => '%y',
+		'a' => '%p',
+		'A' => '%p',
+		'g' => '%I',
+		'G' => '%H',
+		'h' => '%I',
+		'H' => '%H',
+		'i' => '%M',
+		's' => '%S'
+	);
+	
+	/**
+	 * Adding config for output behavoir
+	 */
+	public function editTinyMcePluginLoaderConfig ($arrTinyConfig) {
+		$arrTinyConfig["plugin_insertdate_dateFormat"] = '"' . $this->transformFormat($GLOBALS['TL_CONFIG']['dateFormat']) . '"';
+		$arrTinyConfig["plugin_insertdate_timeFormat"] = '"' . $this->transformFormat($GLOBALS['TL_CONFIG']['timeFormat']) . '"';
+		return $arrTinyConfig;
+	}
+	
+	/**
+	 * Transforms a format from PHP into TinyMCE preferred
+	 */
+	private function transformFormat($strFormat)
+	{
+		$arrFormatTokens = str_split($strFormat);
+		
+		foreach ($arrFormatTokens as $i => $token)
+		{
+			if (ctype_alpha($token))
+			{
+				$arrFormatTokens[$i] = $this->arrMapping[$token];
+			}
+		}
+		
+		return implode('', $arrFormatTokens);
+	}
+}
+ 
+?>
